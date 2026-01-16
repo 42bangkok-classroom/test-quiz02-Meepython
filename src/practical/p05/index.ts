e1a57202af38ba9801dcee8e1fc2102f3939f269
Kankawee Seetalarom <132635060+Meepython@users.noreply.github.com>
2026-01-16 19:36:46 +0700

    fuck my life

--- src/practical/p05/index.ts
+++ src/practical/p05/index.ts
@@ -1,1 +1,52 @@
-export function safeFetchUser() {}
+import axios from "axios"
+
+interface userResult{
+    id: number
+    name: string
+    address: {
+        street: string
+        suite: string
+        city: string
+        zipcode: string
+        geo: {
+            lat: string
+            lng: string
+        }
+    }[]
+}
+
+export const safeFetchUser = async (userId: number): Promise<userResult[]> => {
+    try {
+      const url = `https://api.example.com/users/${userId}`
+      const { data } = await axios.get<userResult[]>(url)
+      if (!data || data.length === 0) {
+        return [];
+      }
+      // This is butthole and bull shit :(
+      const result: userResult[] = []
+      result.push({
+         id: UserId,
+         name: 'string',
+         address: {
+            street: string,
+            suite: string,
+            city: string,
+            zipcode: string,
+            geo: {
+                lat: string,
+                lng: string,
+            }
+        }[]
+      })
+
+      return result
+
+    } catch (error) {
+       if (axios.isAxiosError(error)) {
+          throw new Error(`Failed : ${error.message}`);
+       } else {
+          throw new Error('Unkown Error');
+       }
+    }
+};
+
